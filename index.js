35029045a3952e00b53c9b2bc3ae2c7cd5e52722
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-01-15 00:58:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function submitData(userName,userEmail){
+
+const dataObj = {
+    name : userName,
+    email : userEmail
+}
+
+return  (fetch('http://localhost:3000/users',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            Accept:'application/json'           
+
+        },
+        body:JSON.stringify(dataObj),    
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (data){
+        const id = data.id;
+        const p = document.createElement('p');
+        p.textContent = id;
+        document.body.appendChild(p);
+    }))
+    .catch(function (error){
+        const errorMessage = error.message;
+        const p = document.createElement('p');
+        p.textContent = errorMessage;
+
+        document.body.appendChild(p);
+
+    })
+}
